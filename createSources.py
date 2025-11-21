8f76a8d03bd057b221d01b0b60b84ae0cc3fe82d
heijntje <39136601+heijntje@users.noreply.github.com>
2025-11-21 11:43:56 +0100

    Updates

--- createSources.py
+++ createSources.py
@@ -1,10 +1,21 @@
-import os
 import json
+import os
 
 print("Creating sources.json...")
 
 # List of directories to scan
-directories = ["businessclub", "fivestars", "fourstars", "threestars", "advertisements", "wedstrijdsponsor", "balsponsor", "buffetsponsor", "overige"]
+directories = [
+    "businessclub",
+    "fivestars",
+    "fourstars",
+    "threestars",
+    "advertisements",
+    "announcements",
+    "wedstrijdsponsor",
+    "balsponsor",
+    "buffetsponsor",
+    "overige",
+]
 
 # Empty dictionary to hold our data
 data = {}
@@ -14,17 +25,19 @@ for directory in directories:
     print(f"Scanning {directory}...")
     # Get a list of all files in the directory
     files = os.listdir(os.path.join("public", directory))
-    
+
     # Create a list of dictionaries for each file
     file_dicts = []
     for file in files:
-        if file.endswith('.txt'):
-            with open(os.path.join("public", directory, file), 'r') as f:
+        if file.endswith(".txt"):
+            with open(os.path.join("public", directory, file), "r") as f:
                 content = f.read()
-            file_dicts.append({"name": os.path.splitext(file)[0], "url": ":txt:" + content})
+            file_dicts.append(
+                {"name": os.path.splitext(file)[0], "url": ":txt:" + content}
+            )
         else:
             file_dicts.append({"name": os.path.splitext(file)[0], "url": file})
-    
+
     # Add this list to our data dictionary
     data[directory] = file_dicts
 
